481b5ed9bf198d7b0376b7f7a1de1ebd2dcb1f3b
Marcos Mellado <marcos.mellado@akqa.com>
2019-10-25 15:17:40 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    require.resolve('@contentful/eslint-config-extension'),
+    require.resolve('@contentful/eslint-config-extension/jest'),
+    require.resolve('@contentful/eslint-config-extension/jsx-a11y'),
+    require.resolve('@contentful/eslint-config-extension/react'),
+    'plugin:prettier/recommended'
+  ]
+};
