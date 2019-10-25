d6b35ea8f2692323d9b9431c249f623ce8b33ca9
Marcos Mellado <marcos.mellado@akqa.com>
2019-10-25 17:15:46 +0200

    First features taking headers as instance params

--- src/index.js
+++ src/index.js
@@ -1,74 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  TextInput
+} from '@contentful/forma-36-react-components';
 import { init } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
 
-export class App extends React.Component {
-  static propTypes = {
-    sdk: PropTypes.object.isRequired
+const App = ({ sdk }) => {
+  const columns = sdk.parameters.instance.headers.split(',');
+  const firstCellOfLastRow = useRef(null);
+
+  const getNewRow = () => columns.map(() => '');
+
+  const getRowsFromExistingObject = obj => {
+    return obj.map(row => Object.keys(row).map(key => row[key]));
   };
 
-  detachExternalChangeHandler = null;
+  const getInitialRows = () => {
+    const data = sdk.field.getValue();
+    return data ? getRowsFromExistingObject(data) : [getNewRow()];
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.sdk.field.getValue() || ''
+  const [rows, setRows] = useState(getInitialRows());
+
+  useEffect(() => {
+    sdk.window.startAutoResizer();
+
+    const onExternalChange = value => {
+      const rowData = getRowsFromExistingObject(value);
+      setRows(rowData);
     };
-  }
 
-  componentDidMount() {
-    this.props.sdk.window.startAutoResizer();
+    const detachExternalChangeHandler = sdk.field.onValueChanged(onExternalChange);
 
-    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
-  }
+    return () => {
+      if (detachExternalChangeHandler) {
+        detachExternalChangeHandler();
+      }
+    };
+  }, [sdk]);
 
-  componentWillUnmount() {
-    if (this.detachExternalChangeHandler) {
-      this.detachExternalChangeHandler();
-    }
-  }
-
-  onExternalChange = value => {
-    this.setState({ value });
+  const updateCell = (row, column, value) => {
+    const rowData = rows.slice(0);
+    rowData[row][column] = value;
+    setRows(rowData);
+    sdk.field.setValue(buildTableDataArray());
   };
 
-  onChange = e => {
-    const value = e.currentTarget.value;
-    this.setState({ value });
-    if (value) {
-      this.props.sdk.field.setValue(value);
-    } else {
-      this.props.sdk.field.removeValue();
+  const buildTableDataArray = () => {
+    return rows.reduce((acc, curr) => {
+      acc.push(
+        curr.reduce((a, c, i) => {
+          return { ...a, [columns[i]]: c };
+        }, {})
+      );
+      return acc;
+    }, []);
+  };
+
+  const checkIfLastCell = (e, row, col) => {
+    if (e.keyCode === 9 && row === rows.length - 1 && col === columns.length - 1) {
+      e.preventDefault();
+      const rowData = rows.slice(0);
+      rowData.push(getNewRow());
+      setRows(rowData);
+      // In timeout to allow for new row to render before setting focus
+      setTimeout(() => {
+        firstCellOfLastRow.current.focus();
+      }, 0);
     }
   };
 
-  render() {
-    return (
-      <TextInput
-        width="large"
-        type="text"
-        id="my-field"
-        testId="my-field"
-        value={this.state.value}
-        onChange={this.onChange}
-      />
-    );
-  }
-}
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          {columns.map(h => (
+            <TableCell key={h}>{h}</TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((r, rIdx) => (
+          <TableRow key={`row-${rIdx}`}>
+            {r.map((cell, cIdx) => (
+              <TableCell key={`cell-${rIdx}-${cIdx}`}>
+                <TextInput
+                  type="text"
+                  value={cell}
+                  width="full"
+                  onChange={e => updateCell(rIdx, cIdx, e.target.value)}
+                  onKeyDown={e => checkIfLastCell(e, rIdx, cIdx)}
+                  inputRef={rIdx === rows.length - 1 && cIdx === 0 && firstCellOfLastRow}
+                />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+App.propTypes = {
+  sdk: PropTypes.object.isRequired
+};
 
 init(sdk => {
   ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
 });
-
-/**
- * By default, iframe of the extension is fully reloaded on every save of a source file.
- * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
- */
-// if (module.hot) {
-//   module.hot.accept();
-// }
